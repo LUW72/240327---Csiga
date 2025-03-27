87a741fd2ed5c7fe01e319ce8213095651bd7edb
LUW72 <schneider.ivan337@gmail.com>
2025-03-27 14:56:12 +0100

    alapokszerkezet elkeszitese

--- src/main/Csiga.java
+++ src/main/Csiga.java
@@ -3,5 +3,34 @@ package main;
 
 public class Csiga 
 {
+    int csuszas;
+    boolean bonus;
+    String szin;
+    String szinKod;
+    String csTest;
+
+    public Csiga(String szin, String szinKod)
+    {
+        this.csuszas = 0;
+        this.bonus = false;
+        this.szin = szin;
+        this.szinKod = szinKod;
+        this.csTest = "Ov";
+    }
+    
+    public void csuszasHozzaad(int csusz)
+    {
+        this.csuszas += csusz;
+    }
+    
+    public int getCsuszas(){return csuszas;}
+    public boolean isBonus(){return bonus;}
+    public String getSzin(){return szin;}
+    public String getSzinKod(){return szinKod;}
+    public String getCsTest(){return csTest;}
+    
+    public void setCsuszas(int csuszas){this.csuszas = csuszas;}
+    public void setBonus(boolean bonus){this.bonus = bonus;}
+   
     
 }
--- src/main/Jatek.java
+++ src/main/Jatek.java
@@ -1,7 +1,73 @@
 package main;
 
+import java.util.Random;
+
 
 public class Jatek 
 {
+    public final static Random RND = new Random();
+    
+    int korDb;
+    int kor;
+    int fogadId;
+    final int MAXKORSZAM = 5;
+    Csiga csigak[];
+    String csuszasLista[];
+
+    public Jatek(int korDb)
+    {
+        setKordDb(korDb);
+        this.kor = 1;
+        this.fogadId = 0;
+        
+    }
+    
+    public void setKordDb(int kor)
+    {
+        if(kor > MAXKORSZAM || kor <= 0)
+        {
+            this.korDb = 5;
+        }
+        else
+        {
+            this.korDb = kor;
+        }
+    }
+    
+    public void csigaSzulet()
+    {
+        
+    }
+    
+    public int fogadas()
+    {
+        return 0;
+    }
+    
+    public void megnezKorDb()
+    {
+        
+    }
+    
+    public int csigagyorsito()
+    {
+        return 0;
+    }
+    
+    public String status()
+    {
+        return "";
+    }
+    
+    public void csusznak()
+    {
+        
+    }
+    
+    public void kepernyo()
+    {
+        
+    }
+    
     
 }
