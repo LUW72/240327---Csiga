4dc42b3aac65f79963f0c47bf65619d316c3a078
Viktoria <viktori.zsu@gmail.com>
2025-04-01 13:00:31 +0200

    csigak mennek(tonkre/vilagga)

--- src/main/JatekKezelo.java
+++ src/main/JatekKezelo.java
@@ -3,5 +3,12 @@ package main;
 
 public class JatekKezelo 
 {
-
+    public static void main(String[] args) {
+        Jatek j = new Jatek(5);
+        
+        j.csusznak();
+    }
+    
+    
 }
+
