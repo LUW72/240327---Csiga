8a2a05f2c2c64677c82abec37fc7f6a185e17c05
Viktoria <viktori.zsu@gmail.com>
2025-04-01 12:59:29 +0200

    csigak mennek(tonkre/vilagga)

--- src/main/Jatek.java
+++ src/main/Jatek.java
@@ -1,11 +1,14 @@
 package main;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class Jatek 
 {
-    public final static Random RND = new Random();
+    private final static Random RND = new Random();
+    private final static Scanner SC = new Scanner(System.in);
     
     int korDb;
     int kor;
@@ -17,8 +20,10 @@ public class Jatek
     public Jatek(int korDb)
     {
         setKordDb(korDb);
-        this.kor = 1;
         this.fogadId = 0;
+        this.csigak = new Csiga[3];
+        this.csuszasLista = new String[] {"", "", ""};
+        this.csigaSzulet();
         
     }
     
@@ -36,22 +41,28 @@ public class Jatek
     
     public void csigaSzulet()
     {
-        
+        Csiga piros = new Csiga("piros", "\u001B[31m","Gáspár");
+        this.csigak[0] = piros;
+        Csiga kek = new Csiga("kék", "\u001B[34m", "Bingus");
+        this.csigak[1] = piros;
+        Csiga zold = new Csiga("zöld", "\u001B[32m", "Ernő");
+        this.csigak[2] = zold;
     }
     
-    public int fogadas()
-    {
-        return 0;
+    public void fogadas()
+    {                
     }
     
-    public void megnezKorDb()
-    {
-        
-    }
     
-    public int csigagyorsito()
+    public void csigagyorsito()
     {
-        return 0;
+        int gyorsitsunk = RND.nextInt(0,9);
+        if(gyorsitsunk == 0 || gyorsitsunk == 1){
+            int melyiket = RND.nextInt(0,3);
+            
+            this.csigak[melyiket].setBonus(true);
+            System.out.println(csigak[melyiket].getSzin());
+        }
     }
     
     public String status()
@@ -61,12 +72,59 @@ public class Jatek
     
     public void csusznak()
     {
+        //fogadas();
+        for (int i = 0; i < korDb; i++) {
+            csigagyorsito();
+            for (int j = 0; j < csigak.length; j++) {
+                int mennyit = RND.nextInt(0,4);
+                csigak[j].setCsuszas(mennyit);
+                if(mennyit == 0 && csigak[j].isBonus()){
+                    csuszasLista[j] += "=";
+                }
+                for (int k = 0; k < mennyit; k++) {
+                    
+                    if(csigak[j].isBonus()){
+                        csuszasLista[j] += "==";
+                    }else{
+                        csuszasLista[j] += "_";
+                    }
+                    
+                }
+                this.csigak[j].setBonus(false);
+            }
+            for (int j = 0; j < csuszasLista.length; j++) {
+                System.out.println(csuszasLista[j] + "oV");
+            }
+            System.out.println("Press enter to continue: ");
+            SC.nextLine();
+            
+            //System.out.println(Arrays.toString(csuszasLista));
+            
+        }
         
+        fogadCheck();
+        
+    }
+    
+    public void fogadCheck(){
+        int nyertes = 0;
+        for (int i = 0; i < csuszasLista.length; i++) {
+            if(csuszasLista[i].length()> csuszasLista[nyertes].length()){
+                nyertes = i;
+            }
+        }
+        System.out.println(nyertes);
+        
+        if(fogadId == nyertes){
+            System.out.println("big win!!!");
+        }else{
+            System.out.println("much sad");
+        }
     }
     
     public void kepernyo()
     {
-        
+
     }
     
     
